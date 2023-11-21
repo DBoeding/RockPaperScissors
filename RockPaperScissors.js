61b20ed08600f8bd1945635ffdd6d1eae39c55ec
David <djboeding@gmail.com>
2023-11-21 10:34:22 -0600

    Added User selection Prompt, and changes so only first letter is capitalized.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -4,11 +4,21 @@ console.log("Hello World!")
 
 const choices = ["Rock", "Paper", "Scissors"];
 
-console.log (choices)
 function getComputerChoice(choices) {
     let randomSelection = Math.floor((Math.random() * choices.length));
     let computerChoice = choices[randomSelection];
     return computerChoice
 }
 
-let computerChoice = getComputerChoice(choices);
+let computerSelection = getComputerChoice(choices);
+
+
+function getPlayerChoice(choices){
+    let playerChoice = prompt("Enter your selection: Rock, Paper, Scissors");
+    playerChoice = playerChoice.toLowerCase();
+    playerChoice = playerChoice.charAt().toUpperCase() + playerChoice.slice(1);
+       return playerChoice;
+}
+
+let playerSelection = getPlayerChoice(choices);
+
