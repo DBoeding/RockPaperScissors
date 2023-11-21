1cb0b45a1297ea1513b2ffaad7a9ef037990ab48
David <djboeding@gmail.com>
2023-11-21 09:53:21 -0600

    Added Computers random selection: RPS

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,4 +1,14 @@
 // Rock Paper Scissors
 
-function getComputerChoice() {
+console.log("Hello World!")
+
+const choices = ["Rock", "Paper", "Scissors"];
+
+console.log (choices)
+function getComputerChoice(choices) {
+    let randomSelection = Math.floor((Math.random() * choices.length));
+    let computerChoice = choices[randomSelection];
+    return computerChoice
 }
+
+let computerChoice = getComputerChoice(choices);
