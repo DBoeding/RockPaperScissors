b0b4aa27ae9916a050984e04cb5d3ef1afdf6093
David <djboeding@gmail.com>
2023-11-22 10:29:05 -0600

    Can now determine the winner

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -4,14 +4,18 @@ console.log("Hello World!")
 
 const choices = ["Rock", "Paper", "Scissors"];
 
+let computerSelection = getComputerChoice(choices);
+
+let playerSelection = getPlayerChoice(choices);
+
+playRound(computerSelection, playerSelection);
+
+
 function getComputerChoice(choices) {
     let randomSelection = Math.floor((Math.random() * choices.length));
     let computerChoice = choices[randomSelection];
     return computerChoice
 }
-
-let computerSelection = getComputerChoice(choices);
-
 
 function getPlayerChoice(choices){
     let playerChoice = prompt("Enter your selection: Rock, Paper, Scissors");
@@ -20,5 +24,18 @@ function getPlayerChoice(choices){
        return playerChoice;
 }
 
-let playerSelection = getPlayerChoice(choices);
+function playRound(computerSelection, playerSelection){
+    console.log("Computer choice: " + computerSelection);
+    console.log("Player choice: " + playerSelection);
 
+    if(playerSelection === computerSelection)
+            console.log("Result is a draw");
+    else if(playerSelection === "Rock" && computerSelection === "Scissors")
+            console.log("player Wins");
+    else if(playerSelection === "Paper" && computerSelection === "Rock")
+            console.log("Player Wins");
+    else if(playerSelection === "Scissors" && computerSelection === "Paper")
+            console.log("Player Wins");
+    else
+        console.log("Computer Wins");
+}
